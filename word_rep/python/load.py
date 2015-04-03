22652f1412cf5f7c42bb5dcdea2d02c2874f5508
Zhaoguo Wang <tigerwang1986@gmail.com>
2015-04-03 19:39:31 -0400

    add loader python

---
+++ word_rep/python/load.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# import modules used here -- sys is a very standard one
+import sys
+import math
+import collections
+import scipy.io as sio
+
+def load(N):
+    matfile = sio.loadmat('../data/data.mat', squeeze_me=True, struct_as_record=False)
+    data = matfile['data']
+    numdims = data.trainData.shape[0]
+    D = numdims - 1
+    M = math.floor(data.trainData.shape[1]/N)
+    train_input = data.trainData[0:D, 0:N * M].reshape(D, N, M)
+    train_target = data.trainData[D, 0:N * M].reshape(1, N, M)
+    valid_input = data.validData[0:D, :]
+    valid_target = data.validData[D: D + 1, :]
+    test_input = data.testData[0:D, :]
+    test_target = data.testData[D:D + 1, :]
+    vocab = data.vocab
+    R = collections.namedtuple('data', ['train_input', 'train_target', 'valid_input', 'valid_target', 'test_input', 'test_target', 'vocab'], verbose=True)
+    res = R(train_input, train_target, valid_input, valid_target, test_input, test_target, vocab)
+    return res
+
+
+ # Gather our code in a main() function
+def main():
+    res = load(100)
+    print 'Hello there', res.train_input
+
+
+
+
+# Standard boilerplate to call the main() function to begin
+# the program.
+if __name__ == '__main__':
+    main()
