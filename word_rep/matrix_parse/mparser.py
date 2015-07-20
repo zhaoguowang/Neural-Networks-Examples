141479b6bc3790248819bc365f4892eb46e51dbf
zhaoguo wang <tigerwang1986@gmail.com>
2015-07-19 20:38:26 -0400

    refine paser

--- word_rep/matrix_parse/mparser.py
+++ word_rep/matrix_parse/mparser.py
@@ -2,6 +2,7 @@
 
 # import modules used here -- sys is a very standard one
 import sys, getopt
+import collections
 import os
 import math
 import numpy as np
@@ -22,9 +23,10 @@ def process_float(number):
 
 def process_efloat(number):
 	idx = number.index('e')
-	if number[idx + 1] != '-':
+	if number[idx + 1] != '-' and number[idx + 1] != '+':
+		print number
 		raise NameError("Error: process efloat")
-	return number[idx + 2:]
+	return int(number[idx + 2:])
 
 
 
@@ -38,6 +40,7 @@ def isfloat(value):
 
 def parse_matrix_info(line):
 	if not "epoch" in line or not "batch" in line:
+		print line
 		raise NameError("Error parse matrix info")
 	infos = line.split()
 	# print len(infos)
@@ -49,14 +52,15 @@ def parse_matrix_info(line):
 		idx = info.index(':')
 		# print info
 		if "epoch" in info:
-			epoch = info[idx + 1:]
+			epoch = int(info[idx + 1:])
 		elif "batch" in info:
-			batch = info[idx + 1:]
+			batch = int(info[idx + 1:])
 		elif "row" in info:
-			row = info[idx + 1:]
+			row = int(info[idx + 1:])
 		elif "column" in info:
-			column = info[idx + 1:]
+			column = int(info[idx + 1:])
 		else:
+			print line
 			raise NameError("Error parse matrix info")
 
 	return (epoch, batch, row, column)
@@ -82,20 +86,54 @@ def main(argv):
 	print 'Input file is "', inputfile
 	print 'Output file is "', outputfile
 
+	
+	initial = False
+	precises = {}
+	epoch_precises = {}
+	old_epoch = 0
+
 	with open(inputfile, "r") as infile:
 		for line in infile:
-			if("epoch" in line):
+			
+			if "epoch" in line:
 				(epoch, batch, row, column) = parse_matrix_info(line)
-				print "epoch %s batch %s row %s column %s " %(epoch, batch, row, column)
-			# numbers = line.split()
-			# for num in numbers:
-			# 	if isfloat(num):
-			# 		if 'e' in num:
-			# 			print process_efloat(num)
-			# 		else:
-			# 			print process_float(num)
+				initial = True
+				# print "epoch %d batch %d row %d column %d " %(epoch, batch, row, column)
+				# print precises
+				precises.clear()
 
+				if old_epoch != epoch:
+					print epoch 
+					print epoch_precises
+					epoch_precises.clear()
+					old_epoch = epoch
 
+			elif initial:
+				numbers = line.split()
+
+				for num in numbers:
+					if isfloat(num):
+						p = -1
+						if 'e' in num:
+							p = process_efloat(num)	
+						else:
+							p = process_float(num)
+
+						if p == -1:
+							continue
+
+						if p not in precises:
+							precises[p] = 1
+						else:
+							precises[p] = precises[p] + 1
+
+						if p not in epoch_precises:
+							epoch_precises[p] = 1
+						else:
+							epoch_precises[p] = epoch_precises[p] + 1
+
+	print epoch 
+	print epoch_precises
 
 # Standard boilerplate to call the main() function to begin
 # the program.
